cacb2067b8c609091516704b9f07bbebe3a7b0ca
yzw <994783520@qq.com>
2022-03-08 16:52:27 +0800

    新增管理员实体类

---
+++ src/main/java/com/book/bookshop/entity/Admin.java
@@ -0,0 +1,19 @@
+package com.book.bookshop.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@TableName(value = "bs_admin")
+public class Admin extends Model<Admin> {
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+    private String username;
+    private String password;
+    private String bk;
+}
